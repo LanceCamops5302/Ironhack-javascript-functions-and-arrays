a6ca00e457549cd09e521672c9dec4c2baab5401
Campos Lance <20058@columbushs.com>
2019-11-01 12:01:09 -0400

    second

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,7 +1,32 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return (num1)
+  }
+  else {
+    return (num2)
+  }
+}
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+function findLongestWord(anArray) {
+
+  if (aSingleWord.length === 0){
+    return null
+  }
+
+  longestWord = anArray[0];
+  anArray.forEach( ( aSingleWord ) => {
+    if (aSingleWord.length > longestWord.length) {
+      longestWord = aSingleWord;
+    }
+  })
+  
+  return longestWord;
+}
+
 
 // Iteration #3: Calculate the sum
 
